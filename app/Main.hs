157ec5c0ea43d15eace8266a9230a765f30e1e39
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-02-26 21:40:40 +0100

    Create a Window
    Just to see if the SDL bindings work.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,39 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
+
+import Control.Monad (unless)
+import qualified Data.Text as T
+import SDL
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  initializeAll
+  window <-
+    createWindow (T.pack "Leipe Mocro Tracer") $
+    defaultWindow
+      { windowInputGrabbed = False -- Change this to True when implementing user input
+      , windowOpenGL = Just defaultOpenGL {glProfile = Core Normal 3 3}
+      }
+  _glContext <- glCreateContext window
+  loop window
+
+loop :: Window -> IO ()
+loop window = do
+  events <- pollEvents
+  let shouldQuit =
+        any
+          (\Event {eventPayload} ->
+             case eventPayload of
+               KeyboardEvent KeyboardEventData { keyboardEventKeyMotion
+                                               , keyboardEventKeysym = key
+                                               } ->
+                 keyboardEventKeyMotion == Pressed &&
+                 keysymKeycode key `elem` [KeycodeEscape, KeycodeQ]
+               QuitEvent -> True
+               _ -> False)
+          events
+  glSwapWindow window
+  unless shouldQuit $ loop window
