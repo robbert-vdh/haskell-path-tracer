961e981224d6214afbf2d0111c31c309e2e1fca2
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-07 12:05:36 +0100

    Clean up the objects

--- src/Scene/Objects.hs
+++ src/Scene/Objects.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE DeriveDataTypeable    #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE UndecidableInstances  #-}
-
 module Scene.Objects where
 
--- Accelerate
-import Data.Array.Accelerate as A
-import Data.Array.Accelerate.Linear.Metric
-import Data.Array.Accelerate.Linear.V3
-import Data.Array.Accelerate.Linear.Vector
-
--- standard library
 import Data.Typeable
-import qualified Prelude as P
+import Data.Array.Accelerate as A
+import Data.Array.Accelerate.Linear as A
+import qualified Prelude
 
--- Types
-type Position     = V3 Float
-type Direction    = V3 Float
-type Colour       = V3 Float
+type Position = V3 Float
+
+type Direction = V3 Float
+
+type Colour = V3 Float
+
 type Noraml = (Position, Direction)
 
-data Sphere = Sphere Position Float Colour Float
-    deriving (P.Eq, P.Show, Typeable)
+data Sphere =
+  Sphere Position
+         Float
+         Colour
+         Float
+  deriving (Prelude.Eq, Show, Typeable)
 
-data Plane = Plane Position Direction Colour Float
-    deriving (P.Eq, P.Show, Typeable)
+data Plane =
+  Plane Position
+        Direction
+        Colour
+        Float
+  deriving (Prelude.Eq, Show, Typeable)
