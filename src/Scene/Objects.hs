58e3b5d8bee1238e0ee516bc0222f859b01a1de1
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-10 19:49:04 +0100

    Implement lens getters for spheres and planes

--- src/Scene/Objects.hs
+++ src/Scene/Objects.hs
@@ -8,11 +8,13 @@
 
 module Scene.Objects where
 
-import Data.Typeable
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Array.Sugar
-import Data.Array.Accelerate.Product
+import Data.Array.Accelerate.Control.Lens
 import Data.Array.Accelerate.Linear as A
+import Data.Array.Accelerate.Product
+import Data.Array.Accelerate.Smart
+import Data.Typeable
 import qualified Prelude
 
 -- * Objects
@@ -25,19 +27,51 @@ type Colour = V3 Float
 
 type Noraml = (Position, Direction)
 
-data Sphere =
-  Sphere Position
-         Float
-         Colour
-         Float
-  deriving (Prelude.Eq, Show, Typeable)
+data Sphere = Sphere
+  { _spherePosition :: Position
+  , _sphereRadius :: Float
+  , _sphereColour :: Colour
+  , _sphereSpecularity :: Float
+  } deriving (Prelude.Eq, Show, Typeable)
 
-data Plane =
-  Plane Position
-        Direction
-        Colour
-        Float
-  deriving (Prelude.Eq, Show, Typeable)
+data Plane = Plane
+  { _planePosition :: Position
+  , _planeDirection :: Direction
+  , _planeColour :: Colour
+  , _planeSpecularity :: Float
+  } deriving (Prelude.Eq, Show, Typeable)
+
+-- * Lenses
+--
+-- Since Sphere and Plane do not have a type parameter we can't make use
+-- 'liftLens' or `unlift`, so we'll just define some simple getters ourselves.
+
+class HasPosition t where
+  position :: Getter (Exp t) (Exp Position)
+instance HasPosition Sphere where
+  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx (SuccTupIdx ZeroTupIdx)) `Prj` t
+instance HasPosition Plane where
+  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx (SuccTupIdx ZeroTupIdx)) `Prj` t
+
+class HasColour t where
+  colour :: Getter (Exp t) (Exp Colour)
+instance HasColour Sphere where
+  colour = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
+instance HasColour Plane where
+  colour = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
+
+class HasSpecularity t where
+  specularity :: Getter (Exp t) (Exp Float)
+instance HasSpecularity Sphere where
+  specularity = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+instance HasSpecularity Plane where
+  specularity = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+
+radius :: Getter (Exp Sphere) (Exp Float)
+radius = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
+
+direction :: Getter (Exp Plane) (Exp Direction)
+direction = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
 
 -- * Instances
 -- ** Sphere
