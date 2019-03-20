6b901afc5dc673019a898f468e6b97dbd14a4c11
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-20 16:19:52 +0100

    Refactor the data objects
    - Added a property for illuminance
    - Added a standard material data type
    - Added missing pattern synonyms
    - Got rid of points lights (we don't need them)
    - Unified the lenses

--- src/Scene/Objects.hs
+++ src/Scene/Objects.hs
@@ -32,26 +32,24 @@ type Noraml = (Position, Direction)
 data Scene = Scene
   { _sceneSpheres :: A.Vector Sphere
   , _scenePlanes :: A.Vector Plane
-  , _sceneLights :: A.Vector Light
   } deriving (Typeable)
 
-data Sphere = Sphere
-  { _spherePosition :: Position
-  , _sphereRadius :: Float
-  , _sphereColor :: Color
-  , _sphereSpecularity :: Float
+data Camera = Camera
+  { _cameraPosition :: Position
+  , _cameraDirection :: Direction
+  , _cameraFov :: Int
+  } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
+
+data Material = Material
+  { _materialColor :: Color
+  , _materialSpecularity :: Float
+  , _materialIlluminance :: Float
   } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
 
 data Plane = Plane
   { _planePosition :: Position
   , _planeDirection :: Direction
-  , _planeColor :: Color
-  , _planeSpecularity :: Float
-  } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
-
-data Light = Light
-  { _lightPosition :: Position
-  , _lightColor :: Color
+  , _planeMaterial :: Material
   } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
 
 -- | Any ray that is cast through the scene. This is defined as a type alias as
@@ -63,10 +61,10 @@ data Ray a = Ray
   , _rayDirection :: V3 a
   } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
 
-data Camera = Camera
-  { _cameraPosition :: Position
-  , _cameraDirection :: Direction
-  , _cameraFov :: Int
+data Sphere = Sphere
+  { _spherePosition :: Position
+  , _sphereRadius :: Float
+  , _sphereMaterial :: Material
   } deriving (Prelude.Eq, Show, Typeable, Generic, Elt, IsProduct cst)
 
 -- * Lenses
@@ -79,82 +77,101 @@ data Camera = Camera
 
 makeFields ''Scene
 
-class HasPosition t a | t -> a where
-  position :: Getter (Exp t) (Exp a)
-instance HasPosition Sphere Position where
-  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx (SuccTupIdx ZeroTupIdx)) `Prj` t
-instance HasPosition Plane Position where
-  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx (SuccTupIdx ZeroTupIdx)) `Prj` t
-instance HasPosition Light Position where
-  position = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
-instance HasPosition Camera Position where
-  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
-
 class HasColor t a | t -> a where
   color :: Getter (Exp t) (Exp a)
-instance HasColor Sphere Color where
-  color = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
-instance HasColor Plane Color where
-  color = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
-instance HasColor Light Color where
-  color = to $ \t -> Exp $ ZeroTupIdx `Prj` t
-
-class HasSpecularity t a | t -> a where
-  specularity :: Getter (Exp t) (Exp a)
-instance HasSpecularity Sphere Float where
-  specularity = to $ \t -> Exp $ ZeroTupIdx `Prj` t
-instance HasSpecularity Plane Float where
-  specularity = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+instance HasColor Material Color where
+  color = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
 
 class HasDirection t a | t -> a where
   direction :: Getter (Exp t) (Exp a)
+instance HasDirection Camera Direction where
+  direction = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
 instance HasDirection Plane Direction where
   direction = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
 instance Elt a => HasDirection (Ray a) (V3 a) where
   direction = to $ \t -> Exp $ ZeroTupIdx `Prj` t
-instance HasDirection Camera Direction where
-  direction = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
 
-radius :: Getter (Exp Sphere) (Exp Float)
-radius = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
+class HasFov t a | t -> a where
+  fov :: Getter (Exp t) (Exp a)
+instance HasFov Camera Int where
+  fov = to $ \t -> Exp $ ZeroTupIdx `Prj` t
 
-origin :: Elt a => Getter (Exp (Ray a)) (Exp (V3 a))
-origin = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
+class HasIlluminance t a | t -> a where
+  illuminance :: Getter (Exp t) (Exp a)
+instance HasIlluminance Material Float where
+  illuminance = to $ \t -> Exp $ ZeroTupIdx `Prj` t
 
-fov :: Getter (Exp Camera) (Exp Int)
-fov = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+class HasMaterial t a | t -> a where
+  material :: Getter (Exp t) (Exp a)
+instance HasMaterial Plane Material where
+  material = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+instance HasMaterial Sphere Material where
+  material = to $ \t -> Exp $ ZeroTupIdx `Prj` t
+
+class HasOrigin t a | t -> a where
+  origin :: Getter (Exp t) (Exp a)
+instance Elt a => HasOrigin (Ray a) (V3 a) where
+  origin = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
+
+class HasPosition t a | t -> a where
+  position :: Getter (Exp t) (Exp a)
+instance HasPosition Camera Position where
+  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
+instance HasPosition Plane Position where
+  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
+instance HasPosition Sphere Position where
+  position = to $ \t -> Exp $ SuccTupIdx (SuccTupIdx ZeroTupIdx) `Prj` t
+
+class HasRadius t a | t -> a where
+  radius :: Getter (Exp t) (Exp a)
+instance HasRadius Sphere Float where
+  radius = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
+
+class HasSpecularity t a | t -> a where
+  specularity :: Getter (Exp t) (Exp a)
+instance HasSpecularity Material Float where
+  specularity = to $ \t -> Exp $ SuccTupIdx ZeroTupIdx `Prj` t
 
 -- * Pattern synonyms
 --
 -- See the documentation for 'Data.Array.Accelerate' for more information about
 -- these.
 
+pattern Camera' :: Exp Position -> Exp Direction -> Exp Int -> Exp Camera
+pattern Camera' p d f = Pattern (p, d, f)
+
+pattern Material' :: Exp Color -> Exp Float -> Exp Float -> Exp Material
+pattern Material' c s i = Pattern (c, s, i)
+
+pattern Plane' :: Exp Position -> Exp Direction -> Exp Material -> Exp Plane
+pattern Plane' p d m = Pattern (p, d, m)
+
 pattern Ray' :: Elt a => Exp (V3 a) -> Exp (V3 a) -> Exp (Ray a)
 pattern Ray' o d = Pattern (o, d)
 
-pattern Camera' :: Exp Position -> Exp Direction -> Exp Int -> Exp Camera
-pattern Camera' p d f = Pattern (p, d, f)
+pattern Sphere' :: Exp Position -> Exp Float -> Exp Material -> Exp Sphere
+pattern Sphere' p r m = Pattern (p, r, m)
 
 -- * Instances
 --
 -- The implementations for 'Elt' and 'IsProduct' are derived through 'Generic'.
 
-instance Lift Exp Sphere where
-  type Plain Sphere = Sphere
+instance Lift Exp Camera where
+  type Plain Camera = Camera
   lift = constant
 
 instance Lift Exp Plane where
   type Plain Plane = Plane
   lift = constant
 
-instance Lift Exp Light where
-  type Plain Light = Light
+instance Lift Exp Material where
+  type Plain Material = Material
   lift = constant
 
 instance (Lift Exp a, Elt (Plain a)) => Lift Exp (Ray a) where
   type Plain (Ray a) = Ray (Plain a)
   lift (Ray o d) = Exp $ Tuple $ NilTup `SnocTup` lift o `SnocTup` lift d
 
-instance Lift Exp Camera where
-  type Plain Camera = Camera
+instance Lift Exp Sphere where
+  type Plain Sphere = Sphere
   lift = constant
--- src/Scene/World.hs
+++ src/Scene/World.hs
@@ -6,17 +6,18 @@ import Data.Array.Accelerate as A
 import Linear
 
 getStartCamera :: Camera
-getStartCamera = Camera
+getStartCamera =
+  Camera
    -- { _cameraPosition = V3 1.0 (-1.6) (-4.8)
-   { _cameraPosition = V3 0 0 0
+    { _cameraPosition = V3 0 0 0
    -- TODO: Use proper angles here. This should be implemented using quaternions
    --       and euler angles, but since we don't have any way to rotate the
    --       camera yet we'll just use looking direciton instead.
    -- , _cameraDirection = normalize $ V3 0.3 (-0.3) 0.0
    -- , _cameraDirection = normalize $ V3 (-0.1) (-0.1) (-1.0)
-   , _cameraDirection = normalize $ V3 0 0 (-1)
-   , _cameraFov = 90
-   }
+    , _cameraDirection = normalize $ V3 0 0 (-1)
+    , _cameraFov = 90
+    }
 
 -- | These are the objects and lights used in our static scene.
 getObjects :: Scene
@@ -27,26 +28,42 @@ getObjects =
        [ Sphere
            { _spherePosition = V3 2.0 2.0 (-14.0)
            , _sphereRadius = 5.0
-           , _sphereColor = V3 0.0 1.0 0.0
-           , _sphereSpecularity = 0.7
+           , _sphereMaterial =
+               Material
+                 { _materialColor = V3 0.0 1.0 0.0
+                 , _materialSpecularity = 0.7
+                 , _materialIlluminance = 0.0
+                 }
            }
        , Sphere
            { _spherePosition = V3 6.0 2.0 (-9.0)
            , _sphereRadius = 1.5
-           , _sphereColor = V3 0.0 1.0 0.0
-           , _sphereSpecularity = 0.4
+           , _sphereMaterial =
+               Material
+                 { _materialColor = V3 0.0 1.0 0.0
+                 , _materialSpecularity = 0.4
+                 , _materialIlluminance = 0.0
+                 }
            }
        , Sphere
            { _spherePosition = V3 4.5 1.0 (-9.5)
            , _sphereRadius = 0.3
-           , _sphereColor = V3 0.0 0.0 1.0
-           , _sphereSpecularity = 0.0
+           , _sphereMaterial =
+               Material
+                 { _materialColor = V3 0.0 0.0 1.0
+                 , _materialSpecularity = 0.0
+                 , _materialIlluminance = 1.0
+                 }
            }
        , Sphere
            { _spherePosition = V3 12.0 (-2.4) (-20.0)
-           , _sphereColor = V3 0.5 0.5 0.5
            , _sphereRadius = 0.5
-           , _sphereSpecularity = 0.0
+           , _sphereMaterial =
+               Material
+                 { _materialColor = V3 0.5 0.5 0.5
+                 , _materialSpecularity = 0.0
+                 , _materialIlluminance = 0.0
+                 }
            }
        ])
     (A.fromList
@@ -54,18 +71,21 @@ getObjects =
        [ Plane
            { _planeDirection = V3 0.0 1.0 0.0
            , _planePosition = V3 0.0 (-3.0) 0.0
-           , _planeColor = V3 0.0 0.0 0.0
-           , _planeSpecularity = 0.7
+           , _planeMaterial =
+               Material
+                 { _materialColor = V3 0.0 0.0 0.0
+                 , _materialSpecularity = 0.7
+                 , _materialIlluminance = 0.0
+                 }
            }
        , Plane
            { _planeDirection = V3 0.0 (-1.0) 0.0
            , _planePosition = V3 0.0 10.0 0.0
-           , _planeColor = V3 0.0 0.0 0.0
-           , _planeSpecularity = 0.1
+           , _planeMaterial =
+               Material
+                 { _materialColor = V3 0.0 0.0 0.0
+                 , _materialSpecularity = 0.1
+                 , _materialIlluminance = 0.8
+                 }
            }
-       ])
-    (A.fromList
-       (Z :. 1)
-       [ Light
-           {_lightPosition = V3 0.0 6.0 (-1.0), _lightColor = V3 1.0 1.0 1.0}
        ])
