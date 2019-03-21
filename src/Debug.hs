50631608b7eefc935d27e15a0f95ca20ef62cf6e
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-21 11:52:29 +0100

    Add yet another debug function
    This one uses the interpreter to allow the use of Debug.Trace.

--- src/Debug.hs
+++ src/Debug.hs
@@ -2,6 +2,7 @@ module Debug where
 
 import Data.Array.Accelerate hiding ((++))
 import Data.Array.Accelerate.Linear
+import qualified Data.Array.Accelerate.Interpreter as Interpreter
 import Data.List (intercalate)
 
 import Prelude as P
@@ -9,6 +10,15 @@ import Prelude as P
 import Lib (runN)
 import Scene (render)
 import Util
+
+-- | Compute a single pixel using the interpreter. This allows you to use
+-- 'Debug.Trace' to debug expressions.
+computeDebug :: (Int, Int) -> IO ()
+computeDebug (x, y) = do
+  let pixels = fromList (Z :. 1 :. 1) [(V2 x y, 1)]
+      result = toList $ Interpreter.runN (render theCamera) pixels initialOutput
+
+  putStrLn $ show (x, y) ++ " -> " ++ show result
 
 -- | Compute and print the colour values for the given screen pixels.
 computePixels :: [(Int, Int)] -> IO ()
