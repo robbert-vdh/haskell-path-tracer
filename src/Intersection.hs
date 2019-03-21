9d805a5ae15b8d51b5dd950ab4e0a6d42ee6c84d
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-03-21 12:29:58 +0100

    Add a default implementation for hit

--- src/Intersection.hs
+++ src/Intersection.hs
@@ -8,20 +8,30 @@
 module Intersection where
 
 import Data.Array.Accelerate
+import Data.Array.Accelerate.Control.Lens
 import Data.Array.Accelerate.Data.Maybe
 import Data.Array.Accelerate.Linear
 
 import Scene.Objects
 
-class Primitive p where
+class Primitive p
   -- | Distance to the primitive if it intersects. Returns a 'Just t' if ray
   -- intersects with the primitive at @rayOrigin + rayDIrection * t@.
+  where
   distanceTo :: Exp p -> Exp RayF -> Exp (Maybe Float)
   -- | Get intersection point, normal and material for a sphere hit. Assumes
   -- there is a hit.
   hit :: Exp p -> Exp RayF -> Exp Float -> Exp (RayF, Material)
-
+  -- | Calculate the normal of a primitive at the specified position in world
+  -- space. The normal should be pointing outwards.
   normal :: Exp p -> Exp Position -> Exp Direction
+
+  default hit :: HasMaterial p Material =>
+    Exp p -> Exp RayF -> Exp Float -> Exp (RayF, Material)
+  hit p ~(Ray' o d) t =
+    T2 (Ray' hitPosition (normal p hitPosition)) (p ^. material)
+    where
+      hitPosition = o + (d ^* t)
 
 instance Primitive Sphere where
   distanceTo ~(Sphere' pos rad _) ~(Ray' ori dir) =
@@ -34,9 +44,7 @@ instance Primitive Sphere where
       sep = p - ori
       dist = norm sep - sqrt (rad ** 2 - d_cp ** 2)
 
-  hit = undefined
-
-  normal ~(Sphere' ori _ _) pos = normalize (pos - ori) 
+  normal ~(Sphere' ori _ _) pos = normalize (pos - ori)
 
 instance Primitive Plane where
   distanceTo ~(Plane' pos nor _) ~(Ray' ori dir) =
@@ -46,8 +54,6 @@ instance Primitive Plane where
     where
       x = dir `dot` pos
       dist = ((pos - ori) `dot` nor) / x
-
-  hit = undefined
 
   normal ~(Plane' _ nor _) _ = nor
 
