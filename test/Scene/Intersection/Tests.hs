b55c89e18bcf9e1aa32f5a482a77cabf0e4ce9b6
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-04-17 16:07:42 +0200

    Add sphere backface culling test

--- test/Scene/Intersection/Tests.hs
+++ test/Scene/Intersection/Tests.hs
@@ -23,15 +23,26 @@ sphereTests :: TestTree
 sphereTests =
   testGroup
     "Sphere"
-    [ testProperty "intersection (makeSphere (x, 0, x), x) == (0, 0, x)" $
+    [ testProperty "intersection ((x, 0, x), x) == (0, 0, x)" $
       property $ do
         diameter <- forAll $ Gen.float (Range.linearFrac 0.0 100.0)
-        let sphere = makeSphere (V3 diameter 0 diameter) diameter
+        let sphere = makeSphere (V3 diameter 0.0 diameter) diameter
             ray = Ray' (V3' 0.0 0.0 0.0) (V3' 0.0 0.0 1.0)
             distance = fromJust $ distanceTo ray sphere
             A.T2 (Ray' hitPos _) _ = hit ray distance sphere
 
         evalExp hitPos === V3 0.0 0.0 diameter
+    , testProperty "backface culling" $
+      property $ do
+        diameter <- forAll $ Gen.float (Range.linearFrac 0.1 100.0)
+        ray <-
+          forAll $
+          Ray' (V3' 0.0 0.0 0.0) . normalize . constant <$>
+          v3 (Range.linearFrac (-1.0) 1.0)
+        let sphere = makeSphere (V3 0.0 0.0 0.0) diameter
+
+        -- | Hits to a sphere's backface should not be registered
+        evalExp (distanceTo ray sphere) === Nothing
     ]
 
 -- | Evaluate a single Accelerate expression to a value. This is needed because
@@ -52,3 +63,8 @@ makeSphere pos diameter =
           , _materialBrdf = Diffuse 1.0
           }
     }
+
+-- * Generators
+
+v3 :: Range Float -> Gen (V3 Float)
+v3 r = V3 <$> Gen.float r <*> Gen.float r <*> Gen.float r
